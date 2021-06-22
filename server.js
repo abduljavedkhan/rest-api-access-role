c12070bb68196f579c77b42f95a0b6f5d6e1b444
abduljavedkhan <abduljaved314@gmail.com>
2021-06-23 00:00:29 +0530

    update file

--- server.js
+++ server.js
@@ -76,6 +76,8 @@ app.use(function (err, req, res, next) {
   res.status(500).send(new Response(errorResponse));
 });
 
+const port = process.env.PORT||4321
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
